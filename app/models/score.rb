1ebc7eb522db67295a399644a152b387e53dd59d
Andres Martinez Gutierrez <amartinez0000@gmail.com>
2014-07-14 00:14:50 -0500

    Calculate total points of finalists

--- app/models/score.rb
+++ app/models/score.rb
@@ -38,7 +38,7 @@ class Score < ActiveRecord::Base
 				Score.create(:user_id => f.user_id, :forecast_id => f.id, :points => @points, 
 					:reason => @reason, :type_of_bet => 1)
 
-				if(game.round == 20 && sum_last_two_matches(f.user_id)==0)
+				if(game.round == 20 && 	(self.sum_last_two_matches(f.user))==0)
 					last_two_forecasts = Forecast.where(group: [19,20], user_id: f.user_id)
 
 					@points = Setup.finalists.include?(last_two_forecasts.first.forecast1) ? 5 : 0
@@ -54,6 +54,44 @@ class Score < ActiveRecord::Base
 			end
 			game.update(processed: true)
 			User.prepare_mail(true)
+		end
+	end
+
+	def self.calculate_finalists
+		@finalists = Setup.finalists
+		@forecasts = Forecast.where(group: [19,20])
+
+		@forecasts.each do |f|
+			@points = 0
+			if((@finalists.include?(f.forecast1))||(@finalists.include?(f.forecast2)))
+				index = @finalists.index(f.forecast1)
+				index2 = @finalists.index(f.forecast2)
+
+				if(index==2 && f.group == "19")
+					@points += 10
+					@reason = "Puntos por acertar 3er lugar."
+				elsif(index2==3 && f.group == "19")
+					@points += 8
+					@reason = "Puntos por acertar 4to lugar."
+				elsif(index==0 && f.group == "20")
+					@points += 15
+					@reason = "Puntos por acertar campeón."
+				elsif(index2==1 && f.group == "20")
+					@points += 12
+					@reason = "Puntos por acertar SubCampeón"
+				else
+					@points += 5 unless index.nil?
+					@points += 5 unless index2.nil?
+					@reason = "Total puntos porque #{Setup.teams[f.forecast1]} o #{Setup.teams[f.forecast2]} están en los primeros 4 lugares."
+				end
+			end
+			Rails.logger.info("Usuario: #{f.user.name}. Index: #{index} - Index2: #{index2}. Points: #{@points}")
+
+			if(@points==0)
+				@reason = "No obtuviste puntos con #{Setup.teams[f.forecast1]} o #{Setup.teams[f.forecast2]} en los primeros 4 lugares."
+			end
+
+			Score.create(:user_id => f.user_id, :forecast_id => f.id, :points => @points, :reason => @reason, :type_of_bet => 1)
 		end
 	end
 
